9913e9d054b99bd43393bed53cc673b647b441f0
KevenSoares22 <keven.v.soares@gmail.com>
2023-04-03 19:48:10 -0300

    Criando hook de busca de publicacoes para mostrar na home page

---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,39 @@
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/config';
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [cancelled, setCancelled] = useState(null);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    async function loadData() {
+      if (cancelled) {
+        return;
+      }
+      setLoading(true);
+      const collectionRef = await collection(db, docCollection);
+
+      try {
+        let q;
+        q = await query(collectionRef, orderBy('createdAt', 'desc'));
+        await onSnapshot(q, (querySnapshot) => {
+          setDocuments(querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+          setLoading(false);
+        });
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+  return documents, loading, error;
+};
